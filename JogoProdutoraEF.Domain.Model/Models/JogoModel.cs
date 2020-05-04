9e84b5ac54db5c7a84d6e0bbd2f62d211011400c
Samuel Belo <samuel.belo@al.infnet.edu.br>
2020-05-04 04:30:28 -0300

    Adiciona validações

--- JogoProdutoraEF.Domain.Model/Models/JogoModel.cs
+++ JogoProdutoraEF.Domain.Model/Models/JogoModel.cs
@@ -6,9 +6,12 @@ namespace JogoProdutoraEF.Domain.Models
     public class JogoModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "O nome do jogo é obrigatório.")]
+        [StringLength(maximumLength:60, MinimumLength = 10, ErrorMessage = "Digite no mínimo {2} e no máximo {1} caracteres.")]
         public string Nome { get; set; }
         [DataType(DataType.Date)]
-        
+        [Required(ErrorMessage = "A data de lançamento é obrigatória.")]
+        [Display(Name = "Data de lançamento")]
         public DateTime Lancamento { get; set; }
     }
 }
